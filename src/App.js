4f83a7c345b228aae3953339eaad14953a07412a
Ritik <ritik09052000@gmail.com>
2021-05-12 08:22:44 +0530

    render cocktails using cocktailDB api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React from "react";
 
+//import pages
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import SingleCocktail from "./pages/SingleCocktail";
+
+//import navabar
+import Navbar from "./components/Navbar";
+
+//import react router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 export default function App() {
-  return <h1>app component</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus
+        facere, quas similique quasi expedita quisquam porro illo esse? Rem,
+        architecto? Sapiente, suscipit repudiandae aliquam accusamus cumque
+        numquam porro corrupti nisi.
+      </p>
+    </section>
+  );
 }
